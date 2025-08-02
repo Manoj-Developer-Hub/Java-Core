7350bb206cf59df27959133e1845d26284acccc0
Manoj-Developer-Hub <manojboy562@gmail.com>
2025-08-02 13:09:49 +0530

    Create Hello World Program

---
+++ Hello.java
@@ -0,0 +1,16 @@
+/* 
+
+    1 . Every Java program starts with a class.
+    2 . In Every Main class has a main function.
+    3 . main function is a entry place.
+
+ */
+
+
+
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World !");
+    }
+}
