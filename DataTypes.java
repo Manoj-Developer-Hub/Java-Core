1ab4b78554eb0f6dd29b2b24743620d9880259d9
Manoj <manojboy562@gmail.com>
2025-08-02 13:32:04 +0530

    Data Types in Java

---
+++ DataTypes.java
@@ -0,0 +1,24 @@
+/* 
+
+    Data Types is used to identify the type of Data
+
+    Example
+
+    integer  => stores integer value
+    float   => stores decimla values
+    char    => stores a sigle character surrounded by single quotes
+    boolean => only store true or false
+    string => stores a sequesnce of character surrounded by double quotes
+
+ */
+
+public class DataTypes{
+    public static void main(String[] args) {
+        int num =12;
+        long Ph_no = 1234567890l;
+        float PI =3.14f;
+        char grade = 'A';
+        boolean is_win = true;
+        String name = "Jack MA";
+    }
+}
